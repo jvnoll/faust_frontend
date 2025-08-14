72516ac55d56a594d6967c125ec1674972fb3521
jvnoll <junocence@gmail.com>
2025-08-14 11:55:24 -0400

    feat: implement authentication with NextAuth and CredentialsProvider

---
+++ auth.ts
@@ -0,0 +1,66 @@
+import NextAuth from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const {
+    handlers: { GET, POST },
+    auth,
+    signIn,
+    signOut,
+} = NextAuth({
+    pages: {
+        signIn: "/login",
+    },
+    trustHost: true,
+    secret: process.env.NEXTAUTH_SECRET,
+    providers: [
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                email: { label: 'email', type: "email" },
+                password: { label: 'password', type: "password" },
+            },
+            async authorize(credentials) {
+                const res = await fetch(`${process.env.API_BASE_URL}/auth/login`, {
+                    method: 'post',
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        email: credentials?.email,
+                        password: credentials?.password,
+                    })
+                });
+
+                const data = await res.json();
+
+                if (res.ok && data.token) {
+                    return { 
+                        id: data.user?.id || data.user?.email, 
+                        email: typeof credentials.email === "string" ? credentials.email : undefined,
+                        token: data.token 
+                    };
+                } else {
+                    return null;
+                }
+            },
+        })
+    ],
+    session:{ 
+        strategy: 'jwt'
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.accessToken = user.token;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            session.user = {
+                ...session.user,
+                accessToken: token.accessToken as string,
+            };
+            return session;
+        }
+    }
+});
